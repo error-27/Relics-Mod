2580b9a4473df4481ee694eb0607d230c0ac0b68
[Error:27] <gtdurst@gmail.com>
2022-01-13 20:57:41 -0500

    Setup + First Turret
    - Set up mod.hjson
    - Added Solo turret, a modern version of the Turret from Mindustry Classic

---
+++ src/relics/RelicsMod.kt
@@ -0,0 +1,20 @@
+package relics
+
+import arc.*
+import arc.util.*
+import mindustry.game.EventType.*
+import mindustry.mod.*
+import mindustry.ui.dialogs.*
+import relics.content.*
+
+class RelicsMod : Mod(){
+
+    init{
+        Log.info("Loaded Relics mod constructor.")
+    }
+
+    override fun loadContent(){
+        Log.info("Loading Relics content.")
+        RelicsTurrets().load()
+    }
+}
---
+++ src/relics/content/RelicsTurrets.kt
@@ -0,0 +1,41 @@
+package relics.content
+
+import mindustry.content.Bullets
+import mindustry.content.Fx
+import mindustry.content.Items
+import mindustry.ctype.ContentList
+import mindustry.type.Category
+import mindustry.world.Block
+import mindustry.world.blocks.defense.turrets.ItemTurret
+import mindustry.type.ItemStack.with;
+
+class RelicsTurrets : ContentList {
+
+    override fun load() {
+
+        solo = object : ItemTurret("solo"){}.apply {
+            requirements(Category.turret, with(Items.copper, 35), true)
+            range = 130f
+            reloadTime = 22f
+            ammo(Items.copper, Bullets.standardCopper,
+                Items.graphite, Bullets.standardDense,
+                Items.pyratite, Bullets.standardIncendiary,
+                Items.silicon, Bullets.standardHoming)
+            shots = 1
+            spread = 2f
+            shootCone = 10f
+            ammoUseEffect = Fx.casing1
+            alternate = false
+            restitution = 0.03f
+            health = 250
+            inaccuracy = 1.5f
+            rotateSpeed = 10f
+
+            limitRange()
+        }
+    }
+
+    companion object{
+        lateinit var solo: Block
+    }
+}
